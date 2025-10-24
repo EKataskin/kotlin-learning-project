808d95162fbfb0568b118aba59098018b054a11d
Евгений Катаскин <ynkatask@mts.ru>
2025-10-24 14:50:23 +0300

    fixed build configuration

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -3,17 +3,11 @@
 // It is also used for some aspects of project-wide configuration, like managing plugins, dependencies, etc.
 // https://docs.gradle.org/current/userguide/settings_file_basics.html
 
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}
-
 pluginManagement {
     includeBuild("../build-plugin")
     plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
         id("build-jvm") apply false
         id("build-kmp") apply false
     }
@@ -23,12 +17,20 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
 plugins {
     // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "kotlin-learning-project"
+rootProject.name = "backend"
 
 // Include the `app` and `utils` subprojects in the build.
 // If there are changes in only one of the projects, Gradle will rebuild only the one that has changed.
