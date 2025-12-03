a7bf8963ff7ba7aab6f0f3060a26f6011965a07e
Евгений Катаскин <ynkatask@mts.ru>
2025-12-03 18:16:51 +0300

    add spring dependencies

--- backend/app/build.gradle.kts
+++ backend/app/build.gradle.kts
@@ -1,16 +1,38 @@
 plugins {
     alias { libs.plugins.kotlin.jvm }
+    alias { libs.plugins.spring.boot }
+    alias { libs.plugins.spring.dependencies }
+    alias { libs.plugins.spring.kotlin }
+    alias { libs.plugins.kotlinx.serialization }
     // Apply the Application plugin to add support for building an executable JVM application.
     application
-}
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
     // Project "app" depends on project "utils". (Project paths are separated with ":", so ":utils" refers to the top-level "utils" project.)
     implementation(project(":utils"))
+
+    implementation(libs.spring.actuator)
+    implementation(libs.spring.webflux)
+    implementation(libs.spring.webflux.ui)
+    implementation(libs.jackson.kotlin)
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib"))
+
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Модели
+    implementation(project(":common"))
+
+    // v1 api
+    implementation(project(":api-v1-jackson"))
+    implementation(project(":api-v1-mappers"))
+
+    // tests
+    testImplementation(kotlin("test-junit5"))
+    testImplementation(libs.spring.test)
+    testImplementation(libs.mockito.kotlin)
 }
 
 application {
--- backend/utils/build.gradle.kts
+++ backend/utils/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     alias { libs.plugins.kotlin.jvm }
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     // Apply the kotlinx bundle of dependencies from the version catalog (`../../gradle/libs.versions.toml`).
     implementation(libs.bundles.kotlinxEcosystem)
