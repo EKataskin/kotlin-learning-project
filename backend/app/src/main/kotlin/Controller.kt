4c32f2c0cad6e77d8e49614f96f5696f181b5d67
Евгений Катаскин <ynkatask@mts.ru>
2025-12-04 17:37:12 +0300

    add all methods to controller

--- backend/app/src/main/kotlin/Controller.kt
+++ backend/app/src/main/kotlin/Controller.kt
@@ -2,11 +2,11 @@ package ru.ekataskin.booktracker.app
 
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
-import org.springframework.web.bind.annotation.*
-import ru.ekataskin.booktracker.api.v1.models.BookCreateRequest
-import ru.ekataskin.booktracker.api.v1.models.BookCreateResponse
-import ru.ekataskin.booktracker.api.v1.models.IRequest
-import ru.ekataskin.booktracker.api.v1.models.IResponse
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.ekataskin.booktracker.api.v1.models.*
 import ru.ekataskin.booktracker.common.interfaces.IRequestProcessor
 import ru.ekataskin.booktracker.mappers.fromTransport
 import ru.ekataskin.booktracker.mappers.toTransport
@@ -18,21 +18,34 @@ class Controller(
 ) {
     private val log = KotlinLogging.logger {}
 
-    suspend fun create(@RequestBody request: BookCreateRequest): BookCreateResponse {
-        return process(processor, request, "createBook", log)
-    }
+    @PostMapping("create")
+    suspend fun create(@RequestBody request: BookCreateRequest): BookCreateResponse =
+        processor.process(request, "createBook", log)
 
-    companion object {
-        suspend inline fun <reified Q : IRequest, reified S : IResponse> process(
-            processor: IRequestProcessor,
-            request: Q,
-            logId: String,
-            log: KLogger
-        ): S = processor.processRequest(
-            { fromTransport(request) },
-            { toTransport() as S },
-            logId,
-            log
-        )
-    }
+    @PostMapping("read")
+    suspend fun read(@RequestBody request: BookReadRequest): BookReadResponse =
+        processor.process(request, "readBook", log)
+
+    @PostMapping("update")
+    suspend fun update(@RequestBody request: BookUpdateRequest): BookUpdateResponse =
+        processor.process(request, "updateBook", log)
+
+    @PostMapping("delete")
+    suspend fun  delete(@RequestBody request: BookDeleteRequest): BookDeleteResponse =
+        processor.process(request, "deleteBook", log)
+
+    @PostMapping("search")
+    suspend fun  search(@RequestBody request: BookSearchRequest): BookSearchResponse =
+        processor.process(request, "searchBook", log)
 }
+
+suspend inline fun <reified Q : IRequest, reified S : IResponse> IRequestProcessor.process(
+    request: Q,
+    logId: String,
+    log: KLogger
+): S = this.processRequest(
+    { fromTransport(request) },
+    { toTransport() as S },
+    logId,
+    log
+)
