a51672697656508b9270e1f6952d8d364e57e886
Евгений Катаскин <ynkatask@mts.ru>
2025-12-16 20:09:19 +0300

    add cor test

--- backend/common/build.gradle.kts
+++ backend/common/build.gradle.kts
@@ -6,4 +6,10 @@ plugins {
 dependencies {
     implementation(libs.kotlinx.datetime)
     implementation(libs.bundles.logging)
+
+    // tests
+    implementation(kotlin("test-common"))
+    implementation(kotlin("test-annotations-common"))
+    implementation(kotlin("test-junit"))
+    api(libs.coroutines.test)
 }
